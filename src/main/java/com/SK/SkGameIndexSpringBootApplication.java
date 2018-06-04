ab46de565de12eaf128e7c0b62c73c8d5bff98ea
Dado83 <83deadpool@gmail.com>
2018-06-04 14:12:00 +0200

    create new project

---
+++ src/main/java/com/SK/SkGameIndexSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.SK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkGameIndexSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkGameIndexSpringBootApplication.class, args);
+	}
+}
